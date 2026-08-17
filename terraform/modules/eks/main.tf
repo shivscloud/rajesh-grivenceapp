02e0b7d2045842226cb5187c91b8718734ef4d8b
Rajesh <rajesh@example.com>
2026-08-17 18:20:23 +0000

    changes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -39,7 +39,22 @@ module "eks" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_eks_addon" "coredns"        { cluster_name = module.eks.cluster_name; addon_name = "coredns" }
-resource "aws_eks_addon" "kube_proxy"     { cluster_name = module.eks.cluster_name; addon_name = "kube-proxy" }
-resource "aws_eks_addon" "vpc_cni"        { cluster_name = module.eks.cluster_name; addon_name = "vpc-cni" }
-resource "aws_eks_addon" "ebs_csi_driver" { cluster_name = module.eks.cluster_name; addon_name = "aws-ebs-csi-driver" }
+resource "aws_eks_addon" "coredns" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "coredns"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+}
