92c33279c3c9cd82d99b32d88196ff518cc56c8a
rajesh <rajesh@example.com>
2026-08-17 12:08:01 -0400

    main change

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,45 +1,52 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+	module "eks" {
+	  source  = "terraform-aws-modules/eks/aws"
+	  version = "~> 19.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.28"
+	  cluster_name    = var.cluster_name
+	  cluster_version = "1.28"
 
-  vpc_id                         = var.vpc_id
-  subnet_ids                     = var.private_subnet_ids
-  control_plane_subnet_ids       = var.private_subnet_ids
+	  vpc_id                         = var.vpc_id
+	  subnet_ids                     = var.private_subnet_ids
+	  control_plane_subnet_ids       = var.private_subnet_ids
 
-  cluster_endpoint_public_access = true
+	  cluster_endpoint_public_access = true
 
-  eks_managed_node_groups = {
-    main = {
-      name = "main-node-group"
+	  create_iam_role = false
+	  iam_role_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/eksClusterRole"
 
-      instance_types = ["t3.medium"]
+	  eks_managed_node_groups = {
+	    main = {
+	      name = "main-node-group"
 
-      min_size     = 2
-      max_size     = 4
-      desired_size = 2
-    }
-  }
-}
+	      instance_types = ["t3.medium"]
 
-resource "aws_eks_addon" "coredns" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "coredns"
-}
+	      min_size     = 2
+	      max_size     = 4
+	      desired_size = 2
 
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "kube-proxy"
-}
+	      iam_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AmazonEKSNodeRole"
+	    }
+	  }
+	}
 
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "vpc-cni"
-}
+	data "aws_caller_identity" "current" {}
 
-resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "aws-ebs-csi-driver"
-}
+	resource "aws_eks_addon" "coredns" {
+	  cluster_name = module.eks.cluster_name
+	  addon_name   = "coredns"
+	}
+
+	resource "aws_eks_addon" "kube_proxy" {
+	  cluster_name = module.eks.cluster_name
+	  addon_name   = "kube-proxy"
+	}
+
+	resource "aws_eks_addon" "vpc_cni" {
+	  cluster_name = module.eks.cluster_name
+	  addon_name   = "vpc-cni"
+	}
+
+	resource "aws_eks_addon" "ebs_csi_driver" {
+	  cluster_name = module.eks.cluster_name
+	  addon_name   = "aws-ebs-csi-driver"
+	}
