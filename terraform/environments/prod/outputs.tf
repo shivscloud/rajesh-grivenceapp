61f8ac51eb5a3d0e536272df554769ed7da9c03d
Rajesh <rajesh@example.com>
2026-08-17 16:51:33 +0000

    Infra changes

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -12,3 +12,8 @@ output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc.vpc_id
 }
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
